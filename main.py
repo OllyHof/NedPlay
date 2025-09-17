f17a744fbf5515766048d2c22f17b39c99aba984
OllyHof <Olly.Hof@outlook.com>
2025-09-17 21:30:59 +0200

    Completed controller readout, started on base robot connection

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import math
 
-import pyniryo as bot
+import pyniryo
 import inputs as control
+from pyniryo import NiryoRobot
 
+IP = "192.168.1.10"
+bot = NiryoRobot(IP)
+
+bot.calibrate_auto()
+bot.update_tool()
 Quit = False
 
 for device in control.devices.gamepads:
@@ -21,15 +27,15 @@ while control.devices.gamepads:
         if "ABS_" in event.code:
             if "RX" in event.code:
                 if event.state < 0:
-                    print("Right Stick Left", math.ceil((event.state / 32768) * 100), "%")
+                    print("Right Stick Left", math.ceil((event.state/32768)*-100), "%")
                 else:
-                    print("Right Stick Right", math.ceil((event.state / 32768) * 100), "%")
+                    print("Right Stick Right", math.ceil((event.state/32768)*100), "%")
 
             elif "RY" in event.code:
-                if event.state < 0:
-                    print("Left Stick Right", math.ceil((event.state / 32768) * 100), "%")
+                if event.state > 0:
+                    print("Right Stick Up", math.ceil((event.state/32768)*100), "%")
                 else:
-                    print("Left Stick Left", math.ceil((event.state / 32768) * 100), "%")
+                    print("Right Stick Down", math.ceil((event.state/32768)*-100), "%")
 
             elif "X" in event.code:
                 if event.state == -1:
@@ -39,10 +45,11 @@ while control.devices.gamepads:
                 elif event.state == 0:
                     ()
                 else:
-                    if event.state < 0:
+                    if event.state > 0:
                         print("Left Stick Right", math.ceil((event.state/32768)*100), "%")
                     else:
-                        print("Left Stick Left", math.ceil((event.state / 32768) * 100), "%")
+                        print("Left Stick Left", math.ceil((event.state/32768)*-100), "%")
+
             elif "Y" in event.code:
                 if event.state == 1:
                     print("D-Pad Down")
@@ -51,34 +58,41 @@ while control.devices.gamepads:
                 elif event.state == 0:
                     ()
                 else:
-                    print("Left Stick U/D", math.ceil((event.state/32768)*100), "%")
+                    if event.state > 0:
+                        print("Left Stick Up", math.ceil((event.state/32768)*100), "%")
+                    else:
+                        print("Left Stick Down", math.ceil((event.state/32768)*-100), "%")
+
             elif "_RZ" in event.code:
                 print("Right Trigger", math.ceil((event.state/255)*100), "%")
+
             elif "_Z" in event.code:
                 print("Left Trigger", math.ceil((event.state/255)*100), "%")
 
         if "BTN" in event.code:
-            if "NORTH" in event.code:
-                print("Y")
-            elif "SOUTH" in event.code:
-                print("A")
-            elif "WEST" in event.code:
-                print("X")
-            elif "EAST" in event.code:
-                print("B")
-            elif "TL" in event.code:
-                print("LB")
-            elif "TR" in event.code:
-                print("RB")
-            elif "START" in event.code:
-                print("BACK")
-                Quit = True
-            elif "SELECT" in event.code:
-                print("START")
+            if event.state == 1:
+                if "NORTH" in event.code:
+                    print("Y")
+                elif "SOUTH" in event.code:
+                    print("A")
+                elif "WEST" in event.code:
+                    print("X")
+                elif "EAST" in event.code:
+                    print("B")
+                elif "TL" in event.code:
+                    print("LB")
+                elif "TR" in event.code:
+                    print("RB")
+                elif "START" in event.code:
+                    print("BACK")
+                    Quit = True
+                elif "SELECT" in event.code:
+                    print("START")
 
     if Quit:
         break
 
+bot.close_connection()
 
 
 
