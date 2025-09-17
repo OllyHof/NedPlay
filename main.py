4e99f890df79a03e61b9655a2cc80ee0510dc6c5
OllyHof <olly.hof@outlook.com>
2025-09-17 17:23:51 +0200

    Updated code to read controller inputs and display it to the console

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import time
 import pyniryo as bot
 import inputs as control
 
+Quit = False
+
 for device in control.devices.gamepads:
     print(device)
 
@@ -11,4 +13,38 @@ if not control.devices.gamepads:
 while control.devices.gamepads:
     events = control.get_gamepad()
     for event in events:
-       print(event.ev_type, event.code, event.state)
+        if Quit:
+            break
+        if "ABS_" in event.code:
+            if "RX" in event.code:
+                print("Right Stick L/R")
+            elif "RY" in event.code:
+                print("Right Stick U/D")
+            elif "X" in event.code:
+                print("Left Stick L/R")
+            elif "Y" in event.code:
+                print("Left Stick U/D")
+        if "BTN" in event.code:
+            if "NORTH" in event.code:
+                print("Y")
+            elif "SOUTH" in event.code:
+                print("A")
+            elif "WEST" in event.code:
+                print("X")
+            elif "EAST" in event.code:
+                print("B")
+            elif "TL" in event.code:
+                print("LB")
+            elif "TR" in event.code:
+                print("RB")
+            elif "START" in event.code:
+                print("BACK")
+                Quit = True
+            elif "SELECT" in event.code:
+                print("START")
+    if Quit:
+        break
+
+
+
+
