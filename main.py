c55eb4f27e7b03737b417e3b7d2c6e4764f9e594
OllyHof <olly.hof@outlook.com>
2025-09-23 11:42:55 +0200

    Finished Code, Need to `get higher polling rate, and `try to run code on internal raspberry pi

--- main.py
+++ main.py
@@ -13,16 +13,39 @@ Arm = 0
 Elbow = 0
 Wrist = 0
 Hand = 0
+prev_state = (Base, Shoulder, Arm, Elbow, Wrist, Hand)
 
-# Scale factor for joystick/triggers
-# (joystick values are converted to % and multiplied by SCALE)
-SCALE = 0.01
+# Encoder factor for joystick/triggers
+joystick_encoder_bits = 16
+trigger_encoder_bits = 8
 
+joystick_encoder_bit = math.ceil(math.pow(2, joystick_encoder_bits)/2)
+trigger_encoder_bit = math.pow(2, trigger_encoder_bits)-1
 
 # Limit a value within given min/max joint boundaries
 def lim(n, minn, maxn):
     return max(min(maxn, n), minn)
 
+# Make robot move
+def update_Joint(joystick_state, joint, limitmin, limitmax):
+    if 25 < joystick_state< 50:
+        joint = joint + 0.0296
+    elif 50 < joystick_state < 75:
+        joint = joint + 0.0592
+    elif 75 < joystick_state < 100:
+        joint = joint + 0.0888
+    elif joystick_state == 100:
+        joint = joint + 0.1184
+    elif -50 < joystick_state < -25:
+        joint = joint - 0.0296
+    elif -75 < joystick_state < -50:
+        joint = joint - 0.0592
+    elif -100 < joystick_state < -75:
+        joint = joint - 0.0888
+    elif joystick_state == -100:
+        joint = joint - 0.1184
+    joint = lim(joint, limitmin, limitmax)
+    return joint
 
 # -----------------------------
 # Controller state containers
@@ -61,12 +84,13 @@ D_Pad_State = {
     "ABS_HATOX": 0,  # Horizontal
     "ABS_HATOY": 0,  # Vertical
 }
+prev_button_state = Button_State.copy()
 
 # -----------------------------
 # Connect to robot
 # -----------------------------
 
-IP = "192.168.1.10"
+IP = "169.254.200.200"
 bot = NiryoRobot(IP)
 bot.calibrate_auto()
 bot.update_tool()
@@ -96,47 +120,81 @@ while control.devices.gamepads:
             break
 
         # Joysticks / D-Pad input
-        if "ABS_" in event.code:
-            if not "Z" in event.code:  # Joysticks or D-Pad
-                if event.code in Joystick_State:
-                    # Joystick is converted to % (-100 to 100)
-                    Joystick_State[event.code] = math.ceil(100 * event.state / 32786)
-                else:
-                    D_Pad_State[event.code] = event.state
-
-            elif "Z" in event.code:  # Triggers
-                # Trigger is converted to % (0 to 100)
-                Trigger_State[event.code] = math.ceil(100 * event.state / 255)
+        if event.code in Joystick_State:  # Joysticks or D-Pad
+            # Joystick is converted to % (-100 to 100)
+            Joystick_State[event.code] = math.ceil(100 * event.state / joystick_encoder_bit)
+        if event.code in D_Pad_State:
+            D_Pad_State[event.code] = event.state
+        if event.code in Trigger_State:  # Triggers
+            # Trigger is converted to % (0 to 100)
+            Trigger_State[event.code] = math.ceil(100 * event.state / trigger_encoder_bit)
 
         # Button input
         if "BTN" in event.code:
             Button_State[event.code] = event.state
-            if event.code == "BTN_START" and event.state == 1:
+            if event.code == "BTN_START":
                 Quit = True
 
-    # Send updated joint positions to robot
-    bot.move(JointsPosition(Base, Shoulder, Arm, Elbow, Wrist, Hand))
+
+
 
     # Debug: print actual joint positions
-    print(bot.get_joints())
+    #print(bot.get_joints())
 
     # Update joint values based on joystick/trigger input
-    Base =     lim(Base     + Joystick_State["ABS_RX"] * SCALE, -170, 170)
-    Shoulder = lim(Shoulder + Joystick_State["ABS_RY"] * SCALE, -120, 35)
-    Arm =      lim(Arm      + Joystick_State["ABS_X"]  * SCALE, -77,  90)
-    Elbow =    lim(Elbow    + Joystick_State["ABS_Y"]  * SCALE, -120, 120)
-    Wrist =    lim(Wrist    + Trigger_State["ABS_RZ"]  * SCALE, -100, 55)
-    Hand =     lim(Hand     + Trigger_State["ABS_Z"]   * SCALE, -145, 145)
+    #Base
+    Base = update_Joint(Joystick_State["ABS_RX"], Base, -2.96, 2.96)
+
+    #Shoulder
+    Shoulder = update_Joint(-Joystick_State["ABS_RY"], Shoulder, -2.09, 0.61)
+
+    #Arm
+    Arm = update_Joint(Joystick_State["ABS_Y"], Arm, -1.34, 1.57)
+
+    #Elbow
+    Elbow = update_Joint(Joystick_State["ABS_X"], Elbow, -2.09, 2.09)
+
+    #Wrist
+    Wrist = update_Joint(Trigger_State["ABS_Z"], Wrist, -10.34, 1.57)
+    Wrist = update_Joint(-Trigger_State["ABS_RZ"], Wrist, -10.34, 1.57)
+
 
     # Gripper control using bumpers
     if Button_State["BTN_TR"] == 1:
         bot.grasp_with_tool()
     if Button_State["BTN_TL"] == 1:
         bot.release_with_tool()
+    if Button_State["BTN_SELECT"] == 1:
+        Base = 0
+        Shoulder = 0
+        Arm = 0
+        Elbow = 0
+        Wrist = 0
+        Hand = 0
+    if Button_State["BTN_NORTH"] == 1:
+        Hand = 0
+    if Button_State["BTN_SOUTH"] == 1 and prev_button_state["BTN_SOUTH"] == 0:
+        Hand = Hand/2
+    if Button_State["BTN_EAST"] == 1:
+        Hand = 2.53
+    if Button_State["BTN_WEST"] == 1:
+        Hand = -2.53
+
 
     if Quit:
         break
 
+    prev_button_state = Button_State.copy()
+    # Send updated joint positions to robot
+    new_state = (Base, Shoulder, Arm, Elbow, Wrist, Hand)
+    if new_state != prev_state:
+        bot.move_joints(JointsPosition(*new_state))
+        prev_state = new_state  # save update
+        print("Base: ", Base)
+        print("Shoulder: ", Shoulder)
+        print("Arm: ", Arm)
+        print("Elbow: ", Elbow)
+        print("Wrist: ", Wrist)
 # -----------------------------
 # Close connection when exiting
 # -----------------------------
